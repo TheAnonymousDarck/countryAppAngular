9ef95a57fa5eb89821c8377c61018cb6f951fb78
Enrique Resendiz <k.ventus250@gmail.com>
2023-12-11 21:13:24 -0600

    final project Countries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./shared/pages/home-page/home-page.component";
+import {AboutPageComponent} from "./shared/pages/about/about-page.component";
+import {ContactPageComponent} from "./shared/pages/contact/contact-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{
+    //path: '',
+    //component: HomePageComponent
+  //},
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'countries'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {Country} from "../../interfaces/country.interface";
+import {CountriesService} from "../../services/countries.service";
+import {Region} from "../../types/region.type";
+
+
+@Component({
+  selector: 'app-by-region-page',
+  templateUrl: './by-region-page.component.html',
+  styleUrl: './by-region-page.component.css'
+})
+export class ByRegionPageComponent {
+  public countries: Country[] = [];
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion?: Region;
+
+  constructor( private countriesService: CountriesService ) {}
+
+  searchByRegion( region: Region ):void  {
+    this.selectedRegion = region;
+    this.countriesService.searchRegion( region )
+      .subscribe( countries => {
+        this.countries = countries;
+      });
+
+  }
+}
